c68026f9d6b401847396041c0b61f860f8c12e5d
jose luis <cortezluisjose@gmail.com>
2020-12-07 05:11:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'school_class/new'
+
+  get 'school_class/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
